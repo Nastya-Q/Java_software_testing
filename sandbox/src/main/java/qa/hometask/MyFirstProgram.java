daddd11050ca32c51fdda773bd848860cd0a44ee
Nastya-Q <raed.gyfu@gmail.com>
2018-11-19 16:01:37 +0100

    Calculate distance between 2 points via Point object method

---
+++ sandbox/src/main/java/qa/hometask/MyFirstProgram.java
@@ -0,0 +1,27 @@
+package qa.hometask;
+
+class MyFirstProgram {
+
+    public static void main(String[] args) {
+/*      //Implement via static function
+        Point p1 = new Point (4, 6);
+        Point p2 = new Point (10, 8);
+        System.out.println("Distance via static function:");
+        System.out.println("Distance between point 1 with coordinate (x;y) = (" + p1.x + "; " + p1.y + ")" +
+                "and point2 with coordinate (x;y) = ("  + p2.x + "; " + p2.y + ")" + "equals " + distance(p1, p2) );*/
+
+        //Implement via object method
+        Point p = new Point (4, 6, 10, 8);
+        System.out.println("Distance via object method");
+        System.out.println("Distance between point 1 with coordinate (x1;y1) = (" + p.x1 + ";" + p.y1 + ")" +
+                "and point2 with coordinate (x2;y2) = ("  + p.x2 + ";" + p.y2 + ")" + "equals " + p.distance());
+    }
+
+/*
+    public static double distance(Point p1, Point p2) {
+        double distance = Math.sqrt((Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y),2)));
+        return distance;
+    }
+*/
+
+}
---
+++ sandbox/src/main/java/qa/hometask/Point.java
@@ -0,0 +1,18 @@
+package qa.hometask;
+
+public class Point {
+    public double x1, y1;
+    public double x2, y2;
+
+    public Point (double x1, double y1, double x2, double y2 ) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double distance = Math.sqrt((Math.pow((x1 - x2), 2) + Math.pow((y1 - y2),2)));
+        return distance;
+    }
+}
