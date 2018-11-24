72e9141110a85b9ce8848726381ed7b3127a69be
Nastya-Q <raed.gyfu@gmail.com>
2018-11-24 20:13:08 +0100

    delegated corresponding methods from ApplicationManager class to Group, Contact, Navigation, Session helper classes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -7,9 +7,9 @@ public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.gotoContactEditPage();
-    app.fillContactForm(new ContactData("Ana", "Test", "Moscow, Kremlin 456", "test@qatest.com", "+7951000000"));
-    app.viewCreatedContact();
+    app.getNavigationHelper().gotoContactEditPage();
+    app.getContactHelper().fillContactForm(new ContactData("Ana", "Test", "Moscow, Kremlin 456", "test@qatest.com", "+7951000000"));
+    app.getContactHelper().viewCreatedContact();
   }
 
 }
