4acdd2af55fd9d44eb1a982771d9be0f40e98922
Nastya-Q <raed.gyfu@gmail.com>
2018-12-16 19:31:39 +0100

    added ContactEmailTest to compare phones shown on Home page with phones from Edit page for given contact

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -8,6 +8,9 @@ public class ContactData {
     private String lastName;
     private String address;
     private String email;
+    private String email2;
+    private String email3;
+    private String allEmails;
     private String mobile;
     private String workPhone;
     private String homePhone;
@@ -39,6 +42,14 @@ public class ContactData {
         return email;
     }
 
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
     public String getMobilePhone() {
         return mobile;
     }
@@ -63,8 +74,17 @@ public class ContactData {
         return allPhones;
     }
 
+    public String getAllEmails() {
+        return allEmails;
+    }
+
     public ContactData withAllPhones(String allPhones) {
         this.allPhones = allPhones;
+        return this;
+    }
+
+    public ContactData withAllEmails(String allEmails) {
+        this.allEmails = allEmails;
         return this;
     }
 
@@ -90,6 +110,16 @@ public class ContactData {
 
     public ContactData withEmail(String email) {
         this.email = email;
+        return this;
+    }
+
+    public ContactData withEmail2(String email2) {
+        this.email2 = email2;
+        return this;
+    }
+
+    public ContactData withEmail3(String email3) {
+        this.email3 = email3;
         return this;
     }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactEmailTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
@@ -16,28 +16,23 @@ public class ContactPhoneTests extends TestBase {
         if (app.contact().all().size() == 0) {
             app.goTo().gotoAddContactPage();
             ContactData contact = new ContactData()
-                    .withFirstName("Ana").withLastName("Test").withAddress("Moscow, Kremlin 456").withEmail("test@qatest.com")
+                    .withFirstName("Ana").withLastName("Test").withAddress("Moscow, Kremlin 456")
+                    .withEmail("test@qatest.com").withEmail2("test2@qatest.com").withEmail3("test2@qatest.com")
                     .withMobilePhone("+7(951)000000").withHomePhone("+7 451 000000").withWorkPhone("+7-499-000000").withGroup("test1");
             app.contact().createContact(contact, true);
         }
     }
 
     @Test
-    public void testContactPhones() {
+    public void testContactEmails() {
         app.goTo().homePage();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-        assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
     }
 
-    private String mergePhones(ContactData contact) {
-        return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
-                .stream().filter((s) -> ! s.equals(""))
-                .map(ContactPhoneTests::cleaned)
-                .collect(Collectors.joining("\n"));
-    }
-
-    public static String cleaned(String phone) {
-        return phone.replaceAll("\\s", "" ).replaceAll("[-()]", "");
+    private String mergeEmails(ContactData contact) {
+        return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+                .stream().collect(Collectors.joining("\n"));
     }
 }
