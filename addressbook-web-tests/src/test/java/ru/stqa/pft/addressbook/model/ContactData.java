d269466e4306723ad82c90bbca3779ab83f5eea7
Nastya-Q <raed.gyfu@gmail.com>
2018-12-22 14:57:56 +0100

    updated Contact Data Generator to generate test data to .json file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.io.File;
@@ -8,17 +9,27 @@ import java.util.Objects;
 public class ContactData {
     @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String firstName;
+    @Expose
     private String lastName;
+    @Expose
     private String address;
+    @Expose
     private String email;
+    @Expose
     private String email2;
+    @Expose
     private String email3;
     private String allEmails;
+    @Expose
     private String mobile;
+    @Expose
     private String workPhone;
+    @Expose
     private String homePhone;
     private String allPhones;
+    @Expose
     private String group;
     private File photo;
 
