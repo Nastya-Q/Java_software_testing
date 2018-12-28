66614dc9f21d1295e9134f7fc9b7df2121fa86b0
Nastya-Q <raed.gyfu@gmail.com>
2018-12-28 22:39:00 +0100

    fixed Contact Creation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -136,7 +136,10 @@ public class ContactData {
     }
 
     public File getPhoto() {
-        return new File(photo);
+        if (photo != null) {
+            return new File(photo);
+        }
+        else return null;
     }
 
     public ContactData withPhoto(File photo) {
