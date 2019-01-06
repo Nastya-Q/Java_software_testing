4659a6e8d8511f465b0a889ac22379b064513451
Nastya-Q <raed.gyfu@gmail.com>
2019-01-06 19:00:37 +0100

    trying to fix log in tests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -26,8 +26,23 @@ public class HttpSession {
         httpClient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
     }
 
-    public boolean login (String username, String password) throws IOException {
-        HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
+    public boolean loginWithName(String username, String password) throws IOException {
+        HttpPost post = new HttpPost(app.getProperty("web.baseURL") + "/login_page.php");
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("return", "index.php"));
+        post.setEntity(new UrlEncodedFormEntity(params));
+        CloseableHttpResponse response = httpClient.execute(post);
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == 200) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean loginWithPassword (String username, String password) throws IOException {
+        HttpPost post = new HttpPost(app.getProperty("web.baseURL") + "/login_password_page.php");
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("username", username));
         params.add(new BasicNameValuePair("password", password));
