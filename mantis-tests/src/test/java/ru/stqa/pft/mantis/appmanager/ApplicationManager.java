76ec27096d73777f0be4c8d7c07393be227fc534
Nastya-Q <raed.gyfu@gmail.com>
2019-01-12 13:05:38 +0100

    example of work by FTP protocol (just example, not needed in current tests)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,7 +20,7 @@ public class ApplicationManager {
 
     String browser;
     private RegistrationHelper registrationHelper;
-
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -51,6 +51,13 @@ public class ApplicationManager {
             registrationHelper = new RegistrationHelper(this);
         }
         return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
     }
 
     public WebDriver getDriver() {
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF
+$g_default_timezone       = 'Europe/Berlin';
+$g_crypto_master_salt     = 'Awyl/ygU4A6b+Dc+lWNp94jOnrnuCNaxRN/9I3jSrl4=';
