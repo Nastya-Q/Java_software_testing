eb6af1f77889583232477df1b4a08ee2888cc97a
Nastya-Q <raed.gyfu@gmail.com>
2019-01-12 21:06:32 +0100

    Registration test is done

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -14,6 +14,13 @@ public class RegistrationHelper extends HelperBase {
         wd.get(app.getProperty("web.baseURL") + "/signup_page.php");
         type(By.name("username"), username);
         type(By.name("email"), email);
-        click(By.cssSelector("input[value='Signup']"));
+        click(By.cssSelector("input[type='submit']"));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
     }
 }
